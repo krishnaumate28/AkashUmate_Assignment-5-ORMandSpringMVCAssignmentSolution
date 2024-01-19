a04ddd7e14b30f3fb947b31dd81e2951e53c9010
Akash Umate <akash.umate@corpay.com>
2024-01-19 12:06:24 +0530

    Greded assignment 5 solution commit (#1)

---
+++ Akash Umate- Graded assignment 5 solution /CRM/src/com/gl/akashumate/dao/CustomerDAO.java
@@ -0,0 +1,15 @@
+package com.gl.springdemo.dao;
+
+import java.util.List;
+import com.gl.akashumate.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+}
